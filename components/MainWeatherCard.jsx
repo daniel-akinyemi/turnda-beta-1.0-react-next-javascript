d2b412d33975b742d53c34a20803f966436199ed
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-04-15 16:28:23 +0100

    main-weather-card updated

--- components/MainWeatherCard.jsx
+++ components/MainWeatherCard.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
 
+import { SunIcon,ArrowPathIcon } from '@heroicons/react/24/outline'
 const MainWeatherCard = () => {
   return (
-    <div className="w-full text-white h-40 rounded-md bg-blue-500 p-4">
-      <div className="mwc-bk-1">
+    <div className="w-full text-white text-xs rounded-md bg-blue-500 p-4 space-y-4">
+      <div className="mwc-bk-1 flex justify-between">
         <div>Senin, 20 December 2021</div>
-        <div> 3:30Pm</div>
+        <div> 3:30PM</div>
       </div>
-      <div className="mwc-bk-2">
-        {/* <Image /> */}
-        <div className="mwc-bk-2-1">
-          <div>18° C</div>
-          <div>Hujan Berawan</div>
+
+      <div className="mwc-bk-2 flex space-x-4">
+        <SunIcon className='w-10 h-10'/>
+        <div className="mwc-bk-2-1 text-lg">
+          <div className='font-light'>18° C</div>
+          <div className=' font-bold'>Hujan Berawan</div>
         </div>
       </div>
-      <div className="mwc-bk-3">
+
+      <div className="mwc-bk-3 flex space-x-2 text-xs items-center">
         <div>Terakhir update 3:00PM</div>
-        <button>Refresh</button>
+        <ArrowPathIcon className='h-4 w-4'/>        
       </div>
+
+
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
     <main className='mx-auto max-w-6xl px-4 py-8'>
       <Header/>
       <MainWeatherCard/>
+      Yes
     </main>
   )
 }
