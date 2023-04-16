0f6b2ccb275b8c59defbc6938d34bf8c4d0bd851
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-04-16 16:10:55 +0100

    minor ui improvement - DI page

--- components/DetailedInformation.jsx
+++ components/DetailedInformation.jsx
@@ -5,10 +5,10 @@ const DetailedInformation = () => {
     <section>
       <header className="my-4 text-lg font-medium">Detail Information</header>
       <div className="explained-detail">
-        <div className="px-4 rounded-md py-4 bg-pink-200 flex space-x-4">
-          <CloudIcon className="h-12 w-12" />
-          <div>
-            <div>
+        <div className="px-4 rounded-md py-4 bg-pink-100 flex space-x-4">
+          <CloudIcon className="h-12 w-12 text-orange-500" />
+          <div className="text-xs">
+            <div className="font-bold">
               Cuaca esok hari kemungkinan akan terjadi hujan di siang hari
             </div>
             <div>Jangan lupa bawa payung ya</div>
