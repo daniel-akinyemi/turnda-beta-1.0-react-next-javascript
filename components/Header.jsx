51dab26c30128e1aeb27ee0fc1e08612e24caad6
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-04-18 21:33:39 +0100

    minor updates

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,11 +5,11 @@ const Header = () => {
   return (
     <div className=" mb-4 flex justify-between items-center">
       <MapPinIcon className="h-5 w-5 text-black"/>
-      <div className="flex-grow px-4">
+      <div className="flex-grow px-4 text-sm font-medium">
         {/* A drop down with previously selected addresses */}
         Tanjunslang, Subang
       </div>
-      <MagnifyingGlassIcon className="h-5 w-5"/>
+      <MagnifyingGlassIcon className="h-5 w-5 stroke-[4px]"/>
     </div>
   );
 };
