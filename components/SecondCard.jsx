55eca0185c2cd68c6a703c73e50833dedc556b76
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-04-22 21:58:06 +0100

    minor layout update

--- components/SecondCard.jsx
+++ components/SecondCard.jsx
@@ -3,12 +3,12 @@ const SecondCard = () => {
     <section className="my-4">
       <div className="text-lg font-medium">Cuaca Per Jam</div>
       <div className="flex mt-4 space-x-4 justify-between">
-        <div className="p-4 bg-gray-100 max-w-20% rounded-md">
+        <div className="p-4 bg-gray-100 max-w-20% rounded-xl">
           
           20° C</div>
-        <div className="p-4 bg-gray-100 max-w-20% rounded-md">20° C</div>
-        <div className="p-4 bg-gray-100 max-w-20% rounded-md">20° C</div>
-        <div className="p-4 bg-gray-100 max-w-20% rounded-md">20° C</div>
+        <div className="p-4 bg-gray-100 max-w-20% rounded-xl">20° C</div>
+        <div className="p-4 bg-gray-100 max-w-20% rounded-xl">20° C</div>
+        <div className="p-4 bg-gray-100 max-w-20% rounded-xl">20° C</div>
       </div>
     </section>
   );
