529894a340f4e9f320fe25e85d7eae56940ca63a
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-05-21 19:23:22 +0100

    JSON file created in Main Weather Card (jsx)

--- components/MainWeatherCard.jsx
+++ components/MainWeatherCard.jsx
@@ -1,6 +1,22 @@
 
 import { SunIcon,ArrowPathIcon } from '@heroicons/react/24/outline'
 const MainWeatherCard = () => {
+
+  // const locations = [
+  //   {
+  //     id:1,
+  //     title:'sydney',
+  //   },
+  //   {
+  //     id:2,
+  //     title:'Lagos',
+  //   },
+  //   {
+  //     id:3,
+  //     title:'Paris',
+  //   },
+  // ]
+
   return (
     <div className="w-full text-white text-xs rounded-md bg-blue-500 p-4 space-y-4">
       <div className="mwc-bk-1 flex justify-between">
